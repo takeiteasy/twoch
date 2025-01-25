027082890789ec6e5c1039f7876ce7573d455f02
George Watson <gigolo@hotmail.co.uk>
2025-01-25 15:50:11 +0000

    testing css out

--- src/twoch.lisp
+++ src/twoch.lisp
@@ -2,22 +2,54 @@
 
 (in-package #:twoch)
 
+(defparameter *style*
+  (c:css '((body
+            :display block
+            :margin 8px
+            :padding 0px
+            :background "#C5AD99"
+            :background-image "url(/static/bg.gif)"
+            :font-size 13px
+            :font-family "\"DejaVu Sans\", \"Liberation Sans\", Arial, Verdana, Tahoma, sans-serif"
+            :padding "0 1.5%"
+            :position relative
+            :min-height 97%
+            :color black)
+           ("#title"
+            :text-align center)
+           (".header-inner > h1"
+            :margin 0)
+           (.header
+            :margin-bottom 2em
+            :border-width 1px
+            :border-style outset
+            :padding 6px
+            :border-color gray
+            :background "#CFC")
+           (.header-inner
+            :border-width 1px
+            :border-style inset
+            :padding 6px
+            :border-color gray
+            :display block))))
+
 (with-route ("/" params)
   (declare (ignore params))
   (html-response
    (with-output-to-string (*html*)
      (with-html
-       (:doctype)
+         (:doctype)
        (:html
-         (:head
-           (:title "title"))
-         (:body (:h1 "Hello Common Lisp!")))))))
+        (:head
+         (:title "title"))
+        (:style (:raw *style*))
+        (:body
+         (:div.header#title
+          (:div.header-inner
+           (:h1 "Programming")))
+         (:div.header#thrdlist
+          (:div.header-inner
+           (:span "test")))))))))
 
-(defparameter *static-root*
-  (merge-pathnames #P"static/"
-                   (uiop:pathname-directory-pathname
-                     (or *load-pathname*
-                         *compile-file-pathname*))))
-
-(start :static-root *static-root*
+(start :static-root "/twoch/static/"
        :address "0.0.0.0")
